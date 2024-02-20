4a4b60c09cac332c118610bb2acd83cbf4b39cda
mint73 <ny.snoopy.1164@gmail.com>
2024-02-20 23:15:23 +0900

    change: text

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>Suiran Sell</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://suiranfes.blue"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn SUIRANFES
         </a>
       </header>
     </div>
