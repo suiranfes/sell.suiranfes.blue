3c53c920110a72a29102de29195cfdda17253e8a
mint73 <ny.snoopy.1164@gmail.com>
2024-04-30 16:38:08 +0900

    test: support offline

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +11,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+serviceWorkerRegistration.register();
+reportWebVitals();
