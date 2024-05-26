91030756cab92addb2666bded9796b3f8c723926
nAgI314 <kurosawasyunnagi@gmail.com>
2024-05-26 22:49:45 +0900

    suttleChange

--- src/data.tsx
+++ src/data.tsx
@@ -1,16 +1,16 @@
 //ここに商品のデータを入力してください
 //スペースを入れないように気を付けてください
 export const productData = [
-  { product: "焼きそば", price: "350" },
-  { product: "フランクフルト", price: "200" },
-  { product: "肉巻きおにぎり", price: "350" },
-  { product: "チュロス", price: "250" },
+  { product: "チュロス", price: "200" },
   { product: "かき氷", price: "200" },
+  { product: "ペットボトル", price: "150" },
   { product: "チョコバナナ", price: "300" },
   { product: "つぶつぶアイス", price: "350" },
-  { product: "クレープ", price: "300" },
-  { product: "ドリンク", price: "300" },
-  { product: "ペットボトル", price: "150" },
+  { product: "フランクフルト", price: "200" },
+  { product: "焼きそば", price: "300" },
+  { product: "ドリンク各種", price: "300" },
+  { product: "クレープ", price: "350" },
+  { product: "肉巻きおにぎり", price: "400" },
   //{product:"与謝野晶子",price:"1700"}
   //{product:"与謝野晶子mk.2",price:"17000000"}
 ];
