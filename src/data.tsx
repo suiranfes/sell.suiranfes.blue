afbada76c358853d236a6c06caf88e62e239b126
nAgI314 <kurosawasyunnagi@gmail.com>
2024-05-28 19:02:33 +0900

    aisutyokobananaaaaaaaaaaaa

--- src/data.tsx
+++ src/data.tsx
@@ -4,7 +4,7 @@ export const productData = [
   { product: "チュロス", price: "200" },
   { product: "かき氷", price: "200" },
   { product: "ペットボトル", price: "150" },
-  { product: "チョコバナナ", price: "300" },
+  { product: "アイスチョコバナナ", price: "300" },
   { product: "つぶつぶアイス", price: "350" },
   { product: "フランクフルト", price: "200" },
   { product: "焼きそば", price: "300" },
