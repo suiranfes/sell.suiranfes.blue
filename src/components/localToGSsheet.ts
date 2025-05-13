2c2d983c3caf1a4e838864fe8d9b759defeca00f
nAgI314 <kurosawasyunnagi@gmail.com>
2025-05-13 18:03:25 +0900

    delete GAS

--- src/components/localToGSsheet.ts
+++ src/components/localToGSsheet.ts
@@ -5,35 +5,38 @@ const localStorageLib = new LocalStorageLib();
 let isRequestInProgress = false;
 
 const reflectLocal = async () => {
-  if (localStorage.getItem("isUser") == "true" && !isRequestInProgress) {
-    const GAS_URL = import.meta.env.VITE_GAS_API_URL;
+  //通信が混み合ってしまった時に、スプレッドシートに送れていないデータを保存しておいて、そういったデータだけ一気に送る何かを作る？
 
-    isRequestInProgress = true;
 
-    // ラベル作成
-    const to_top_data: string[] = ["UTC", "時間"];
-    for (let i = 0; i < productData.length; i++) {
-      to_top_data.push(productData[i].product);
-    }
+  // if (localStorage.getItem("isUser") == "true" && !isRequestInProgress) {
+  //   const GAS_URL = import.meta.env.VITE_GAS_API_URL;
 
-    const local_all_data = localStorageLib.local_all_array();
-    // const labeles_all_data = local_all_data.unshift(to_top_data);
-    // console.log(local_all_data);
+  //   isRequestInProgress = true;
 
-    try {
-      await fetch(GAS_URL, {
-        method: 'POST',
-        mode: 'no-cors',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ ID: localStorage.getItem("ID"), data: local_all_data })
-      });
-    } finally {
-      isRequestInProgress = false;
-    }
-  }
-  else {
-    // console.log("現在保存中です。");
-  }
+  //   // ラベル作成
+  //   const to_top_data: string[] = ["UTC", "時間"];
+  //   for (let i = 0; i < productData.length; i++) {
+  //     to_top_data.push(productData[i].product);
+  //   }
+
+  //   const local_all_data = localStorageLib.local_all_array();
+  //   // const labeles_all_data = local_all_data.unshift(to_top_data);
+  //   // console.log(local_all_data);
+
+  //   try {
+  //     await fetch(GAS_URL, {
+  //       method: 'POST',
+  //       mode: 'no-cors',
+  //       headers: { 'Content-Type': 'application/json' },
+  //       body: JSON.stringify({ ID: localStorage.getItem("ID"), data: local_all_data })
+  //     });
+  //   } finally {
+  //     isRequestInProgress = false;
+  //   }
+  // }
+  // else {
+  //   // console.log("現在保存中です。");
+  // }
 };
 
 export { reflectLocal };
